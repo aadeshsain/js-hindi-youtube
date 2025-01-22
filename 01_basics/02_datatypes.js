95096c8ddff947dc99313119ca3baf304d670637
aadeshsain <sainaadesh1121@gmail.com>
2025-01-22 10:46:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.JS not browser
+
+// console.log(3 
+//     + 
+//     3);// code readability should be high
+
+// console.log("aadesh sain"); 
+
+
+// let name = "aadesh" //string datatypes === ""
+// let age = 18; //number datatypes // number => 2 to power 53
+// let isloggedIn = true; // boolean datatypes === true & false 
+// let myName; // undefined datatypes === the value not define yet 
+// let myNum = null; // null datatypes stand alone value
+// let youNum = 133223923293928392828398329832n; // bigInt datatypes
+// let symbol = $ ;   // symbol datatypes === get a unique value
+
+
+
+// object 
+
+// console.log(typeof undefined);  === undefined
+// console.log(typeof null);   === null
+
