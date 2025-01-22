dc3eeee8472a85f890e0c498c860bf47986b6826
aadeshsain <sainaadesh1121@gmail.com>
2025-01-22 11:15:57 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// // console.log(score);
+
+//"33" => 33
+// "33abs" => NaN
+// true => 1 ;; false => 0
+
+
+// let isloggedIn = ""
+
+// let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(typeof (booleanisloggedIn));
+// console.log( booleanisloggedIn);
+
+
+// let islogggedIn = 9
+
+// let booleanislogggedIn = Boolean(islogggedIn)
+// console.log(typeof (booleanislogggedIn));
+// console.log( booleanislogggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "aadesh" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+
+
+console.log( stringNumber);
+console.log(typeof stringNumber);
